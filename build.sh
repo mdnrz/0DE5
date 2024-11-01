12e4c5b931f4a7a0b6458aa5143fb476d828a5ef
mehdi-norouzi <mehdeenoroozi@gmail.com>
2024-11-01 23:46:43 +0330

    wtf is .exe?

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p bin
-as --64 -g -o ./bin/rpn.o ./stack/postfix/rpn.s && ld -m elf_x86_64 ./bin/rpn.o -o ./bin/rpn.exe
-as --64 -g -o ./bin/wcx64e.o ./wcx64/wcx64e.s && ld -m elf_x86_64 ./bin/wcx64e.o -o ./bin/wcx64e.exe
+as --64 -g -o ./bin/rpn.o ./stack/postfix/rpn.s && ld -m elf_x86_64 ./bin/rpn.o -o ./bin/rpn.elf
+as --64 -g -o ./bin/wcx64e.o ./wcx64/wcx64e.s && ld -m elf_x86_64 ./bin/wcx64e.o -o ./bin/wcx64e.elf
